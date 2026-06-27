38ff83231212737e3840af41837049577ecb8e64
Kingfarii112477 <mr.shadow11247@gmail.com>
2026-06-27 11:13:37 +0500

    capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -4,7 +4,6 @@ const config: CapacitorConfig = {
   appId: 'ai.echoverse.app',
   appName: 'EchoVerse AI',
   webDir: 'out',
-  bundledWebRuntime: false,
   server: {
     androidScheme: 'https',
     // For development: comment this out for production builds
